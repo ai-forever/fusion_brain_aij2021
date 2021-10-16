20e285b3d88d70199ec6f616481b7b69d79ab3ea
Daria Bakshandaeva <43073018+Gugutse@users.noreply.github.com>
2021-10-16 19:39:40 +0300

    loss

---
+++ fb_baseline/fb_utils/loss.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+
+
+def onehot(size, target):
+    vec = torch.zeros(size, dtype=torch.float32)
+    vec[target] = 1.
+    return vec
+
+
+class LabelSmoothing(nn.Module):
+    def __init__(self, smoothing = 0.1):
+        super(LabelSmoothing, self).__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+
+    def forward(self, x, target):
+        if self.training:
+            x = x.float()
+            target = target.float()
+            logprobs = torch.nn.functional.log_softmax(x, dim = -1)
+            nll_loss = -logprobs * target
+            nll_loss = nll_loss.sum(-1)
+            smooth_loss = -logprobs.mean(dim=-1)
+            loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+            return loss.mean()
+        else:
+            return torch.nn.functional.cross_entropy(x, target)
